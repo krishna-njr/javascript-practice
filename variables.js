37d1b9e5e9061cdb29d5f774d61638f3d9a52fa2
keshav <keshav@gmail.com>
2026-05-28 17:34:14 +0530

    variables and scope is done

---
+++ variables.js
@@ -0,0 +1,33 @@
+
+// Three type of variable in javascript : 
+
+/*
+    var : function 
+    let : block 
+    const : block 
+
+*/ 
+
+
+let fname = 'username', lname = 'lastname', age = 24; 
+
+console.log(`
+        first name of user is ${fname}
+        last name of user is ${lname}
+        and age of person is ${age}
+    `); 
+
+
+const printUser = () => {
+    let fname = 'username', lname = 'lastname', age = 24; 
+    if(fname){
+        let fname = 'username-2'; 
+        // const location  = 'xyz/street-123'; 
+        var location  = 'xyz/street-123'; 
+        console.log(fname); 
+    }
+    console.log(`location of user is ${location}`); 
+
+}
+
+printUser(); 
